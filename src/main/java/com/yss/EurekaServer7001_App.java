a3234437d2e105eb0fb5e3df7a6304a11901639e
yinhongzhang <1410801173@qq.com>
2019-02-01 15:37:00 +0800

    Eureka-server

--- src/main/java/com/yss/EurekaServer7001_App.java
+++ src/main/java/com/yss/EurekaServer7001_App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 @SpringBootApplication
 @EnableEurekaServer    // EurekaServer服务器端启动类,接受其它微服务注册进来,指定它作为服务注册中心
 @EnableWebSecurity     // 启动祖册中心开启验证功能
